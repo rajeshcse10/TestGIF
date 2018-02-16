96da145de5858278db70ca33a9b3181069c185d0
Rajesh Karmaker <rajeshkarmaker0@gmail.com>
2018-02-16 19:20:41 +0900

    Initial Commit

---
+++ TestGIF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGIF
+//
+//  Created by Rajesh's MacBook Pro  on 16/2/18.
+//  Copyright © 2018 MacBook Pro Retina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGIF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGIF
+//
+//  Created by Rajesh's MacBook Pro  on 16/2/18.
+//  Copyright © 2018 MacBook Pro Retina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
